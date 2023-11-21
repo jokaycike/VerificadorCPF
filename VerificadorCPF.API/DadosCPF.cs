e903bb6440033a90f50d6e5af18a51f6ec3e0796
Joana Luiza <joanasilva@uniaraxa.edu.br>
2023-11-21 17:22:03 -0300

    21/11/2023

--- VerificadorCPF.API/DadosCPF.cs
+++ VerificadorCPF.API/DadosCPF.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Text.RegularExpressions;
 
 namespace VerificadorCPF.API
 {
@@ -6,34 +7,46 @@ namespace VerificadorCPF.API
     {
         public DateTime? Data { get; set; }
         public bool? Valido { get; set; }
-        public string? Mensagem { get; set;}
+        public string? Mensagem { get; set; }
         public string? Estado { get; set; }
         public string? CPF { get; set; }
 
-        public static DadosCPF TodaVerificacao(string CPF) 
-        { 
+        public static DadosCPF TodaVerificacao(string CPF)
+        {
             DadosCPF OBJ = new DadosCPF();
             RetornoResposta NullOk = NullVerificacao(CPF);
             if (NullOk.Valido == false)
             {
                 OBJ.Valido = NullOk.Valido;
                 OBJ.Mensagem = NullOk.Mensagem;
-                return OBJ;   
-
+                return OBJ;
             }
+
             RetornoResposta TamanhoOk = TamanhoVerificacao(CPF);
-            if(TamanhoOk.Valido == false)
+            if (TamanhoOk.Valido == false)
             {
                 OBJ.Valido = TamanhoOk.Valido;
                 OBJ.Mensagem = TamanhoOk.Mensagem;
                 return OBJ;
             }
+
+            RetornoResposta ApenasOK = ApenasVerificacao(CPF);
+            if (ApenasOK.Valido == false)
+            {
+                OBJ.Valido = ApenasOK.Valido;
+                OBJ.Mensagem = ApenasOK.Mensagem;
+                return OBJ;
+            }
+
+            RetornoResposta LimpaOK = LimpaVerificacao(CPF);
+
             OBJ.Mensagem = "oiii";
             return OBJ;
         }
+
         private static RetornoResposta NullVerificacao(string CPF)
         {
-            if(CPF == null)
+            if (CPF == null)
             {
                 return new RetornoResposta
                 {
@@ -50,9 +63,10 @@ namespace VerificadorCPF.API
                 };
             }
         }
+
         private static RetornoResposta TamanhoVerificacao(string CPF)
         {
-            if(CPF.Length >= 11 && CPF.Length <= 14)
+            if (CPF.Length == 11 || CPF.Length == 14)
             {
                 return new RetornoResposta
                 {
@@ -70,6 +84,55 @@ namespace VerificadorCPF.API
             }
         }
 
+        private static RetornoResposta ApenasVerificacao(string CPF)
+        {
+            if (Regex.IsMatch(CPF, @"^\d{11}$|^\d{14}$|^\d{3}\.\d{3}\.\d{3}-\d{2}$"))
+            {
+                return new RetornoResposta
+                {
+                    Valido = true,
+                    Mensagem = ""
+                };
+            }
+            else
+            {
+                return new RetornoResposta
+                {
+                    Valido = false,
+                    Mensagem = "Confira se não está enserindo letra ou simbolos ou esta colocando a pontuação no lugar errado"
+                };
+            }
 
+        }
+
+        private static RetornoResposta LimpaVerificacao(string CPF)
+        {
+            string CPFLimpo = "";
+
+            foreach (char i in CPF)
+            {
+                try
+                {
+                    CPFLimpo += Convert.ToString(Convert.ToInt32(i));
+                }
+                catch
+                {
+                }
+            }
+            return new RetornoResposta
+            {
+                Valido = true,
+                Mensagem = CPFLimpo
+            };
+        }
+
+        private static RetornoResposta ContaVerificacao(string CPF)
+        {
+            return new RetornoResposta
+            {
+                Valido = true,
+                Mensagem = CPF
+            };
+        }
     }
 }
